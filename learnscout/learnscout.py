d389b5b2c1bd8540c2b7cca3dd36026d86f807e4
Rene Mueller <rene.mueller@code.berlin>
2020-04-13 02:57:16 +0200

    new structure, flask implemented, templates running, static sites established

--- learnscout/learnscout.py
+++ learnscout/learnscout.py
@@ -1,20 +1,40 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
+@app.route('/layout')
+def layout():
+    return render_template('layout.html')
+
+@app.route('/index')
+def index():
+    return render_template('index.html')
 
 @app.route('/')
+@app.route('/welcome')
+@app.route('/learnscout')
 def welcome():
-    return '<h1>hednwuencllo world</h1>'
+    return render_template('welcome.html')
 
 @app.route('/getguidance')
 def get_guidance():
-    return '<h1>Get Guidance</h1>'
+    return render_template('getguidance.html', title='Get Guidance')
 
 @app.route('/login')
 @app.route('/logout')
 def login():
-    return '<h1>Please log-in here</h1>'
+    return render_template('login.html', title='Login')
 
+@app.route('/myupskillingplan')
+def myupskillingplan():
+    return render_template('myupskillingplan.html', title='My Upskilling Plan')
+
+@app.route('/startupskilling')
+def startupskilling():
+    return render_template('startupskilling.html', title='Start Upskilling')
+
+@app.route('/material')
+def material():
+    return render_template('material.html', title='Material for Upskilling')
 
 if __name__ == '__main__':
     app.run(debug=True)
